025e838cbf5ab76da6e35eef6a0b21169ee96b5b
Sulier_Jim <sulier@sulier-jimdemac-mini.local>
2019-02-19 14:35:58 +0800

    init project

---
+++ ssss/ssss/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ssss
+//
+//  Created by Sulier_Jim on 2019/2/19.
+//  Copyright © 2019年 LShaun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ssss/ssss/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ssss
+//
+//  Created by Sulier_Jim on 2019/2/19.
+//  Copyright © 2019年 LShaun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
